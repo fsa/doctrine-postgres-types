7834bbb8669b6b18b425b30343513d16b90ca380
Sergei Fedotov <fsa@tavda.info>
2024-03-29 15:46:55 +0700

    Добавлен тип Jsonb

---
+++ src/Doctrine/DBAL/PostgresTypes/BaseType.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\DBAL\PostgresTypes;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Abstraction on top of Doctrine default Type class.
+ *
+ * @since 0.1
+ *
+ * @author Martin Georgiev <martin.georgiev@gmail.com>
+ */
+abstract class BaseType extends Type
+{
+    protected const TYPE_NAME = null;
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        $this->throwExceptionIfTypeNameNotConfigured();
+
+        return $platform->getDoctrineTypeMapping(static::TYPE_NAME);
+    }
+
+    public function getName(): string
+    {
+        $this->throwExceptionIfTypeNameNotConfigured();
+
+        return static::TYPE_NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return false;
+    }
+
+    private function throwExceptionIfTypeNameNotConfigured(): void
+    {
+        if (null === static::TYPE_NAME) {
+            throw new \LogicException(\sprintf('Doctrine type defined in class %s has no meaningful value for TYPE_NAME constant', self::class));
+        }
+    }
+}
---
+++ src/Doctrine/DBAL/PostgresTypes/JsonbType.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\DBAL\PostgresTypes;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * Implementation of PostgreSql JSONB data type.
+ *
+ * @see https://www.postgresql.org/docs/9.4/static/datatype-json.html
+ * @since 0.1
+ *
+ * @author Martin Georgiev <martin.georgiev@gmail.com>
+ */
+class Jsonb extends BaseType
+{
+    use JsonTransformer;
+
+    /**
+     * @var string
+     */
+    protected const TYPE_NAME = 'jsonb';
+
+    /**
+     * Converts a value from its PHP representation to its database representation of the type.
+     *
+     * @param array|bool|float|int|string|null $value the value to convert
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        return $this->transformToPostgresJson($value);
+    }
+
+    /**
+     * Converts a value from its database representation to its PHP representation of this type.
+     *
+     * @param string|null $value the value to convert
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): null|array|bool|float|int|string
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        return $this->transformFromPostgresJson($value);
+    }
+}
